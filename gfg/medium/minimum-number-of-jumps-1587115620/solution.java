ace8305f90dde49d5ee8b3e9fedf7e6d57f4d927
NiranjanSaravanakumar <nickniranjan2929@gmail.com>
2026-07-15 10:05:24 +0530

    ✅ Minimum Jumps | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-15

--- gfg/medium/minimum-number-of-jumps-1587115620/solution.java
+++ gfg/medium/minimum-number-of-jumps-1587115620/solution.java
@@ -1,30 +1,27 @@
 class Solution {
     public int minJumps(int[] nums) {
-
         int n = nums.length;
 
-        if (n == 1)
-            return 0;
+        if (n == 1) return 0;
+        if (nums[0] == 0) return -1;
 
-        if (nums[0] == 0)
-            return -1;
-
+        int l = 0, r = 0;
         int jumps = 0;
-        int end = 0;
-        int farthest = 0;
 
-        for (int i = 0; i < n - 1; i++) {
+        while (r < n - 1) {
 
-            farthest = Math.max(farthest, i + nums[i]);
+            int farthest = r;
 
-            if (i == end) {
-
-                if (farthest == end)
-                    return -1;
-
-                jumps++;
-                end = farthest;
+            for (int i = l; i <= r; i++) {
+                farthest = Math.max(farthest, i + nums[i]);
             }
+
+            if (farthest == r)
+                return -1;
+
+            l = r + 1;
+            r = farthest;
+            jumps++;
         }
 
         return jumps;
