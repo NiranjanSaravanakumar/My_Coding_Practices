b2260450c7c53f9b085771f4a000c13168b64a79
NiranjanSaravanakumar <nickniranjan2929@gmail.com>
2026-07-15 10:18:51 +0530

    ✅ Array Leaders | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-15

--- gfg/easy/leaders-in-an-array-1587115620/solution.java
+++ gfg/easy/leaders-in-an-array-1587115620/solution.java
@@ -1,17 +1,23 @@
 class Solution {
     static ArrayList<Integer> leaders(int arr[]) {
-       ArrayList<Integer> res = new ArrayList<>();
-       int n = arr.length;
-       int curmax = arr[n-1];
-       res.add(curmax);
-       
-       for(int i=n-2;i>=0;i--){
-           if(curmax <= arr[i]){
-               curmax = arr[i];
-               res.add(curmax);
-           }
-       }
-       Collections.reverse(res);
+
+        ArrayList<Integer> res = new ArrayList<>();
+
+        int n = arr.length;
+        int curmax = arr[n - 1];
+
+        res.add(curmax);
+
+        for (int i = n - 2; i >= 0; i--) {
+
+            if (arr[i] >= curmax) {
+                curmax = arr[i];
+                res.add(curmax);
+            }
+        }
+
+        Collections.reverse(res);
+
         return res;
     }
 }
