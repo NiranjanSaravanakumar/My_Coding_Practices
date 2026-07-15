53ff5070c16e05bc33a9bb17e89f5f6371e0906d
NiranjanSaravanakumar <nickniranjan2929@gmail.com>
2026-07-15 11:07:14 +0530

    ✅ Climbing Stairs | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 42.1 MB | 2026-07-15

---
+++ leetcode/easy/0070-climbing-stairs/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        int a = 0,b=1,res = 0;
+
+        for(int i=0;i<n;i++){
+            res = a + b;
+            a = b;
+            b =res;
+        }
+        return res;
+    }
+}
