4c7c7c265471f6f281349adc62d5f66c26891589
NiranjanSaravanakumar <nickniranjan2929@gmail.com>
2026-07-16 22:44:38 +0530

    ✅ ERROR404 - Rating 267 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-16

--- codechef/easy/ERROR404---Rating-267/solution.java
+++ codechef/easy/ERROR404---Rating-267/solution.java
@@ -7,13 +7,13 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
+		int x = sc.nextInt();
 		
-		while(t-- >0){
-		    int x = sc.nextInt();
-		    int y = sc.nextInt();
-		    
-		    System.out.println((x/10)*y);
+		if( x == 404){
+		    System.out.println("NOT FOUND");
+		}
+		else{
+		    System.out.println("FOUND");
 		}
 
 	}
