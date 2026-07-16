1969e5b80213dfef7b9af67bb49c6d4c01f1edb2
NiranjanSaravanakumar <nickniranjan2929@gmail.com>
2026-07-16 21:23:20 +0530

    ✅ Sum of GCD of Formed Pairs | Medium | Java | Runtime: 56 ms (66.50%) | Memory: 108 MB | 2026-07-16

--- leetcode/medium/3867-sum-of-gcd-of-formed-pairs/solution.java
+++ leetcode/medium/3867-sum-of-gcd-of-formed-pairs/solution.java
@@ -2,25 +2,33 @@ class Solution {
     public long gcdSum(int[] nums) {
         int n = nums.length;
         int[] prefixGcd = new int[n];
+
         int max = 0;
         for (int i = 0; i < n; i++) {
             max = Math.max(max, nums[i]);
             prefixGcd[i] = gcd(nums[i], max);
         }
+
         Arrays.sort(prefixGcd);
+
         long ans = 0;
         int i = 0, j = n - 1;
+
         while (i < j) {
             ans += gcd(prefixGcd[i], prefixGcd[j]);
             i++;
             j--;
         }
+
         return ans;
     }
 
-    private int gcd(int a, int b) {
-        if (b == 0)
-            return a;
-        return gcd(b, a % b);
+    public int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 }
