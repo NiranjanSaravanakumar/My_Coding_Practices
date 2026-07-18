d9132157eb14632350fb9bda4d7e37586ce8d682
NiranjanSaravanakumar <nickniranjan2929@gmail.com>
2026-07-18 20:39:37 +0530

    ✅ Q2. Maximum Value of an Alternating Sequence | Medium | Java | Runtime: 0 ms | Memory: 42.3 MB | 2026-07-18

---
+++ leetcode/medium/maximum-value-of-an-alternating-sequence/solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public long maximumValue(int n, int s, int m) {
+        if(n ==1)
+            return s;
+        long peeks = n/2;
+        return s + m + (peeks -1)*(m-1);
+    }
+}
