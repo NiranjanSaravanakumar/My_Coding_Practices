56f10188fe0a100d0228dfe7c8ee3b168000aeef
NiranjanSaravanakumar <nickniranjan2929@gmail.com>
2026-07-20 22:04:23 +0530

    ✅ FCTPR | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-20

--- codechef/medium/FCTPR/solution.java
+++ codechef/medium/FCTPR/solution.java
@@ -4,41 +4,9 @@ import java.io.*;
 
 class Codechef
 {
-    static boolean canpro(long time ,int [] a,long x){
-        long total =0;
-        for(int t:a){
-            total += time/t;
-            if(total >= x)
-                return true;
-        }
-        return false;
-    }
-    
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int n = sc.nextInt();
-		long x = sc.nextInt();
-		
-		int [] a = new int [n];
-		int mintime = Integer.MAX_VALUE;
-		for(int i=0;i<n;i++){
-		    a[i] = sc.nextInt();
-		    mintime = Math.min(mintime,a[i]);
-		}
-		long = l = 0;
-		long h = (lon) mintime * x;
-		long ans = h;
-		
-		while(l<=h){
-		    long m = l+(h-l)/2;
-		    if(canpro(m,a,x)){
-		        ans = m;
-		        h = m -1;
-		    }
-		    else{
-		        l = mid +1;
-		    }
-		}
-        System.out.println(ans);
+		// your code goes here
+
 	}
 }
