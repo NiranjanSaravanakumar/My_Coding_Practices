b0f00a30ca7b7aa7ba89ab493b5f1a9cbf66993b
NiranjanSaravanakumar <nickniranjan2929@gmail.com>
2026-07-21 20:01:48 +0530

    ✅ Maximize Active Section with Trade I | Medium | Java | Runtime: 144 ms (25.35%) | Memory: 48.8 MB | 2026-07-21

---
+++ leetcode/medium/3499-maximize-active-section-with-trade-i/solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxActiveSectionsAfterTrade(String s) {
+        int ones  =0 ;
+
+        for(char c:s.toCharArray()){
+            if (c == '1'){
+                ones++;
+            }
+        }
+        String t = "1" + s + "1";
+        ArrayList<Character> ch = new ArrayList<>();
+        ArrayList<Integer> len = new ArrayList<>();
+
+        int i =0;
+        while(i<t.length()){
+            char c = t.charAt(i);
+            int j =i;
+            while(j<t.length() && c==t.charAt(j)){
+                j++;
+            }
+            ch.add(c);
+            len.add(j-i);
+            i = j;
+        }
+        int maxgain = 0;
+
+        for(int k=1;k<ch.size()-1;k++){
+            if(ch.get(k)=='1' && ch.get(k-1) == '0' && ch.get(k+1) == '0'){
+                int gain = len.get(k-1) + len.get(k+1);
+                maxgain = Math.max(maxgain,gain);
+            }
+        }
+        return ones + maxgain;
+    }
+}
