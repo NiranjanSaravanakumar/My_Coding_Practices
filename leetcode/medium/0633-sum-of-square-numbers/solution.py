d41e4984c059b7e5ce06aedb75ba948617814835
NiranjanSaravanakumar <nickniranjan2929@gmail.com>
2026-07-21 09:05:26 +0530

    ✅ Sum of Square Numbers | Medium | Python | Runtime: 0 ms | Memory: 19.1 MB | 2026-07-21

---
+++ leetcode/medium/0633-sum-of-square-numbers/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        l = 0
+        r = int(sqrt(c))
+
+        while l<= r:
+            sum = l*l + r*r
+            if sum == c:
+                return True
+            elif sum < c:
+                l += 1
+            else:
+                r -= 1
+        return False
