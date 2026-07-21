d544a9743f9e5e25b4e5d7af6694d9ef2968c791
NiranjanSaravanakumar <nickniranjan2929@gmail.com>
2026-07-21 20:04:02 +0530

    ✅ Maximize Active Section with Trade I | Medium | Python | Runtime: 1175 ms (19.62%) | Memory: 21.6 MB | 2026-07-21

--- leetcode/medium/3499-maximize-active-section-with-trade-i/solution.py
+++ leetcode/medium/3499-maximize-active-section-with-trade-i/solution.py
@@ -2,26 +2,24 @@ class Solution:
     def maxActiveSectionsAfterTrade(self, s: str) -> int:
         ones = s.count('1')
 
-        t = "1" + s + "1"
+        t = '1' + s + '1'
 
         ch = []
         length = []
 
         i = 0
         while i < len(t):
-            c = t[i]
             j = i
-            while j < len(t) and t[j] == c:
+            while j < len(t) and t[j] == t[i]:
                 j += 1
-            ch.append(c)
+            ch.append(t[i])
             length.append(j - i)
             i = j
 
-        max_gain = 0
+        maxGain = 0
 
         for k in range(1, len(ch) - 1):
-            if ch[k] == '1' and ch[k - 1] == '0' and ch[k + 1] == '0':
-                gain = length[k - 1] + length[k + 1]
-                max_gain = max(max_gain, gain)
+            if ch[k] == '1' and ch[k-1] == '0' and ch[k+1] == '0':
+                maxGain = max(maxGain, length[k-1] + length[k+1])
 
-        return ones + max_gain
+        return ones + maxGain
