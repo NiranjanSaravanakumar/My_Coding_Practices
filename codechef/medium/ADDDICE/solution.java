0c5d9b594e083b31aa92141e6186cb71023426a1
NiranjanSaravanakumar <nickniranjan2929@gmail.com>
2026-07-22 20:19:42 +0530

    ✅ ADDDICE | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-22

---
+++ codechef/medium/ADDDICE/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		if(t== 3 || t== 4|| t== 5 || t == 6){
+		    System.out.println("YES");
+		}
+		else{
+		    System.out.println("NO");
+		}
+
+	}
+}
