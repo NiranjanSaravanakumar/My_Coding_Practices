55b44d6d983c2d8f2c44b387158f4bdce6029d82
NiranjanSaravanakumar <nickniranjan2929@gmail.com>
2026-07-22 19:55:16 +0530

    ✅ GSQ14 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-07-22

---
+++ codechef/medium/GSQ14/solution.sql
@@ -0,0 +1,8 @@
+/* Write a query which does the following
+- Delete the row where the department is Client.
+- Output the entire table.
+*/
+
+Delete from employee where Department='Client';
+
+select * from employee;
