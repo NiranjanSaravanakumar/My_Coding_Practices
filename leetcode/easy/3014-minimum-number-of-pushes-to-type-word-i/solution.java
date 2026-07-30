374587de1bb6938532b98b00edda6665894f6a31
NiranjanSaravanakumar <nickniranjan2929@gmail.com>
2026-07-30 12:01:37 +0530

    ✅ Minimum Number of Pushes to Type Word I | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 43.1 MB | 2026-07-30

--- leetcode/easy/3014-minimum-number-of-pushes-to-type-word-i/solution.java
+++ leetcode/easy/3014-minimum-number-of-pushes-to-type-word-i/solution.java
@@ -1,15 +1,11 @@
 class Solution {
     public int minimumPushes(String word) {
-        int curpush =1;
-        int res = 0;
-        int count =0;
-        for(char c:word.toCharArray()){
-            if(count>0 && count%8==0){
-                curpush++;
-            }
-            res += curpush;
-            count++;
-        }
-        return res;
+        int n = word.length();
+        int rem = n % 8;
+        int comp = n / 8;
+
+        int ans = 8 * (comp * (comp + 1) / 2) + (comp + 1) * rem;
+
+        return ans;
     }
 }
