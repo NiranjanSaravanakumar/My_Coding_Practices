1b02b3b2044f81b60add9c91a700b4112160fbdb
NiranjanSaravanakumar <nickniranjan2929@gmail.com>
2026-09-06 18:57:54 +0530

    ✅ Distinct Subsequences | Hard | Java | Runtime: 20 ms (53.25%) | Memory: 66.5 MB | 2026-09-06

---
+++ leetcode/hard/0115-distinct-subsequences/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+
+        int m = s.length();
+        int n = t.length();
+
+        // dp[i][j] = number of ways to form t[j...]
+        // using s[i...]
+
+        long[][] dp = new long[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][n] = 1;
+        }
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (s.charAt(i) == t.charAt(j)) {
+
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                } else {
+                    dp[i][j] = dp[i + 1][j];
+                }
+            }
+        }
+        return (int) dp[0][0];
+    }
+}
